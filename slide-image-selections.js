3094f71b2554d9b3c30b51384ea031fa8f314272
Otto <vonwao@gmail.com>
2026-03-28 15:47:05 -0400

    Seed default slide image selections

--- slide-image-selections.js
+++ slide-image-selections.js
@@ -1,6 +1,67 @@
 const SLIDE_IMAGE_SELECTIONS = {
   version: 1,
-  slides: {},
+  slides: {
+    title: {
+      candidateId: 'bild01'
+    },
+    opening: {
+      candidateId: 'bild05'
+    },
+    herford: {
+      candidateId: 'bild03'
+    },
+    source: {
+      candidateId: 'bild13'
+    },
+    ruins: {
+      candidateId: 'bild14'
+    },
+    trust: {
+      candidateId: 'bild30'
+    },
+    receive: {
+      candidateId: 'bild26'
+    },
+    incurable: {
+      candidateId: 'bild20'
+    },
+    mystery: {
+      candidateId: 'bild23'
+    },
+    echoes: {
+      candidateId: 'bild25'
+    },
+    thoughts: {
+      candidateId: 'bild37'
+    },
+    love: {
+      candidateId: 'bild31'
+    },
+    stakes: {
+      candidateId: 'bild40'
+    },
+    'free-will': {
+      candidateId: 'bild25'
+    },
+    personal: {
+      candidateId: 'bild37'
+    },
+    found: {
+      candidateId: 'bild22'
+    },
+    pause: {
+      candidateId: 'bild31'
+    },
+    closing: {
+      candidateId: 'bild39'
+    },
+    invitation: {
+      candidateId: 'bild48'
+    },
+    thanks: {
+      candidateId: 'bild21'
+    }
+  },
 };
 
 if (typeof window !== 'undefined') {
