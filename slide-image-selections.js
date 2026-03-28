8dd9705b88645eea9f22332525baaa5af5ccb2fc
Otto <vonwao@gmail.com>
2026-03-28 17:46:53 -0400

    Stabilize image selector saves

--- slide-image-selections.js
+++ slide-image-selections.js
@@ -1,67 +1,71 @@
 const SLIDE_IMAGE_SELECTIONS = {
-  version: 1,
-  slides: {
-    title: {
-      candidateId: 'bild01'
+  "version": 1,
+  "slides": {
+    "title": {
+      "candidateId": "bild01",
+      "focusY": 24,
+      "focusX": 35,
+      "scale": 1,
+      "dim": 0.18
     },
-    opening: {
-      candidateId: 'bild05'
+    "opening": {
+      "candidateId": "bild05"
     },
-    herford: {
-      candidateId: 'bild03'
+    "herford": {
+      "candidateId": "bild03"
     },
-    source: {
-      candidateId: 'bild13'
+    "source": {
+      "candidateId": "bild13"
     },
-    ruins: {
-      candidateId: 'bild14'
+    "ruins": {
+      "candidateId": "bild14"
     },
-    trust: {
-      candidateId: 'bild30'
+    "trust": {
+      "candidateId": "bild30"
     },
-    receive: {
-      candidateId: 'bild26'
+    "receive": {
+      "candidateId": "bild26"
     },
-    incurable: {
-      candidateId: 'bild20'
+    "incurable": {
+      "candidateId": "bild20"
     },
-    mystery: {
-      candidateId: 'bild23'
+    "mystery": {
+      "candidateId": "bild23"
     },
-    echoes: {
-      candidateId: 'bild25'
+    "echoes": {
+      "candidateId": "bild25"
     },
-    thoughts: {
-      candidateId: 'bild37'
+    "thoughts": {
+      "candidateId": "bild37"
     },
-    love: {
-      candidateId: 'bild31'
+    "love": {
+      "candidateId": "bild31"
     },
-    stakes: {
-      candidateId: 'bild40'
+    "stakes": {
+      "candidateId": "bild40"
     },
-    'free-will': {
-      candidateId: 'bild25'
+    "free-will": {
+      "candidateId": "bild25"
     },
-    personal: {
-      candidateId: 'bild37'
+    "personal": {
+      "candidateId": "bild37"
     },
-    found: {
-      candidateId: 'bild22'
+    "found": {
+      "candidateId": "bild22"
     },
-    pause: {
-      candidateId: 'bild31'
+    "pause": {
+      "candidateId": "bild31"
     },
-    closing: {
-      candidateId: 'bild39'
+    "closing": {
+      "candidateId": "bild39"
     },
-    invitation: {
-      candidateId: 'bild48'
+    "invitation": {
+      "candidateId": "bild48"
     },
-    thanks: {
-      candidateId: 'bild21'
+    "thanks": {
+      "candidateId": "bild21"
     }
-  },
+  }
 };
 
 if (typeof window !== 'undefined') {
